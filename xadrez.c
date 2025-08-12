2f1af7be718a69f01b27c4638fd993f271b2a884
Samuel <108034309+samguardian@users.noreply.github.com>
2025-08-12 22:53:11 +0000

    feat: implementação de funções recursivas

--- xadrez.c
+++ xadrez.c
@@ -5,62 +5,64 @@ void movimentarBispo();
 void movimentarRainha();
 void movimentarCavalo();
 
+const size_t ENTRY_POINT = 0;
+
 int main()
 {
     printf("Movimentação torre:\n");
-    movimentarTorre();
-    printf("\nMovimentação bispo:\n");    
-    movimentarBispo();
+    movimentarTorre(ENTRY_POINT);
+    printf("\nMovimentação bispo:\n");
+    movimentarBispo(ENTRY_POINT);
     printf("\nMovimentação rainha:\n");
-    movimentarRainha();
+    movimentarRainha(ENTRY_POINT);
     printf("\nMovimentação cavalo:\n");
-    movimentarCavalo();
+    movimentarCavalo(ENTRY_POINT);
     return 0;
 }
 
 // Seu programa deverá simular o movimento da Torre cinco casas para a direita. Move-se em linha reta horizontalmente ou verticalmente. Seu programa deverá simular o movimento da Torre cinco casas para a direita.
-void movimentarTorre()
+void movimentarTorre(size_t i)
 {
-    for (size_t i = 0; i < 5; i++)
+    if (i < 5)
     {
         printf("Direita\n");
+        movimentarTorre(++i);
     }
+    
 }
 
 // Move-se na diagonal. Seu programa deverá simular o movimento do Bispo cinco casas na diagonal para cima e à direita. Para representar a diagonal, você imprimirá a combinação de duas direções a cada casa (ex: "Cima, Direita").
-void movimentarBispo()
-{
-    size_t move = 0;
-    while (move < 5)
+void movimentarBispo(size_t i)
+{   
+    if (i < 5)
     {
         printf("Cima, Direita\n");
-        move++;
+        movimentarBispo(++i);
     }
 }
 
 // Move-se em todas as direções. Seu programa deverá simular o movimento da Rainha oito casas para a esquerda.
-void movimentarRainha()
-{
-    size_t move = 0;
-    do
+void movimentarRainha(size_t i)
+{    
+    if (i < 8)
     {
         printf("Esquerda\n");
-        move++;
-    } while (move <= 8);
+        movimentarRainha(++i);
+    }
 }
 
 // Mova-se em L. Seu programa deverá simular o movimento do Cavalo duas casas para baixo e uma para esquerda.
-void movimentarCavalo()
+void movimentarCavalo(size_t i)
 {
-    size_t j = 0;
-    for (size_t i = 0; i < 1; i++)
+    if (i < 1)
     {
-        while (j < 2)
+        while (i < 2)
         {
-            printf("Baixo\n");
-            j++;
+            printf("Cima\n");
+            ++i;
         }
 
-        printf("Esquerda\n");
+        printf("Direita\n");
+        movimentarCavalo(++i);
     }
 }
